12f71661c3b14265de418faa192030491a3ac2d5
GustavoSwDaniel <gustavodanieldetoledo@gmail.com>
2024-10-31 14:24:50 -0300

    Fix: Remove verfication data

--- main.py
+++ main.py
@@ -93,10 +93,3 @@ response = data_process(schema=schema, data_list=facts)
 
 for resp in response:
     print(resp)
-
-
-('gabriel', 'endereço', 'av rio branco, 109', True),
-('joão', 'endereço', 'rua bob, 88', True),
-('joão', 'telefone', '91234-5555', True),
-('gabriel', 'telefone', '98888-1111', True),
-('gabriel', 'telefone', '56789-1010', True)
